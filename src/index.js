1650c161def70a8c893bbb68ce05db01bd910078
amit-rai1 <raiamit9264@gmail.com>
2023-09-25 17:17:28 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
 import './index.css';
 // import axios from 'axios'; // Import axios at the top
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 if (process.env.NODE_ENV === 'production') {
   disableReactDevTools();
@@ -18,15 +19,8 @@ if (process.env.NODE_ENV === 'production') {
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    
-    <App />
-   
+      <App />
     </BrowserRouter>
   </React.StrictMode>
-  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
