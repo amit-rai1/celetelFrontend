49e08cae500a808113b254e25e6a7af0b321cb19
celtel-1 <amit.rai@celetel.com>
2023-10-16 22:42:29 +0530

    remove console

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom"
-// import AddForm from './features/AddForm';
 import Login from './features/Login';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import HomeDashboard from './features/Dashboard/HomeDashboard';
 import Sidebar from './features/Common/Sidebar';
-import EmpList from './features/EmpList/EmployeList';
-// import DataList from './features/DataList/DataList';
-// import HeaderAdmin from './features/Header/Sidebar';
-// import PrivateRoute from './features/authRoute/PrivateRoute';
-import DataList from './features/DataList/DataList'
 import { OperatorDetail } from './features/SimDataList';
 import SimDataForm from './features/SimData';
 import SenderIDComponent from './features/SenderIDComponent';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -28,21 +21,16 @@ function App() {
 
         <Route path="/login" element={ <Login/> } />
 
-        {/* <Route path="/" element={ <AddForm/> } /> */}
-                    {/* <Route path="/AddForm" element={ <AddForm/> } />  */}
                      <Route path="/" element={ <HomeDashboard/> } /> 
                      <Route path="/details" element={ <OperatorDetail/> } />
                      <Route path="/addForm" element={ <SimDataForm/> } />
 
 
                     <Route path="/Sidebar" element={ <Sidebar/> } /> 
-                    <Route path="/EmpList" element={ <EmpList/> } /> 
-                    <Route path="/dataList" element={ <DataList/> } />  
                     <Route path="/senderid" element={ <SenderIDComponent/> } />  
 
 
 
-                    {/* <PrivateRoute path="/AddForm" element={<AddForm />} /> */}
 
 
         </Routes>
